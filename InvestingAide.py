afef38440982dcf6cc647e466c86ceaf0effeb56
Smayan Ranjan <smayanranjan2303@gmail.com>
2024-12-16 15:21:14 -0500

    Clarified that it only accepts a ticker and not the company name

--- InvestingAide.py
+++ InvestingAide.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 st.title('Investing Aide')
 st.caption('A Stock Dashboard to help you find all stock metrics and help with technical analysis')
-ticker = st.sidebar.text_input('Ticker', 'AAPL')
+ticker = st.sidebar.text_input('Ticker(Not Company Name)', 'AAPL')
 today = datetime.today()
 one_year_back = today - timedelta(days=371)
 startDate = st.sidebar.date_input("Start Day (At least 1 Year Ago)", max_value=one_year_back)
